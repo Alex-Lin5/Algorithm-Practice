1747f4b4b55d7627467c72c74cd14036ae38f8ca
TigerBig <jizlin575@gmail.com>
2022-09-13 12:11:38 -0400

    found maze delete bug

--- HW1/HW1.cpp
+++ HW1/HW1.cpp
@@ -5,12 +5,23 @@
 #include <queue>
 #include <map>
 #include <bitset>
-#include <filesystem>
+// #include <filesystem>
+#include <windows.h>
+#include <tchar.h>
+
+#define BUFSIZE MAX_PATH
 
 using namespace std;
 
 class Cell;
 class Maze;
+void MY_DFS_iterative(Maze& maze, ofstream& out);
+void MY_DFS_recursive(Maze& maze, Cell* cell);
+void MY_DFS_recursive_wrap(Maze& maze, ofstream& out);
+void MY_BFS(Maze& maze, ofstream& out);
+void DFS_iterative(vector<Cell>& maze, int numC, int start, int end);
+void DFS_recursive(vector<Cell>& maze, int numC, int start, int end, bool done);
+void BFS(vector<Cell>& maze, int numC, int start, int end);
 
 class Cell {
 	public:
@@ -19,9 +30,11 @@ class Cell {
 		bool visited;
 		Cell* from;
 
+		Cell();
 		Cell(const int id, const int wall);
 		~Cell(){ delete from;}
-		Cell* neighbor(Maze& maze); // return unvisited neighbor
+		vector<Cell*> neighbor(Maze& maze); // return unvisited neighbor
+		friend ostream& operator<<(ostream& str, const Cell* C);
 };
 
 class Maze {
@@ -38,19 +51,15 @@ class Maze {
 		Maze(int row, int col);
 		~Maze();
 		void reset();
-		void print_path(ostream& out);
-		void print_solution(ostream& out);
+		bool boundary(const int id);
+		void print_path(ofstream& out);
+		void print_solution(ofstream& out);
 };
 
-void MY_DFS_iterative(Maze& maze, ostream& out);
-void MY_DFS_recursive(Maze& maze, Cell* cell);
-void MY_DFS_recursive_wrap(Maze& maze, ostream& out);
-void MY_BFS(Maze& maze, ostream& out);
-void DFS_iterative(vector<Cell>& maze, int numC, int start, int end);
-void DFS_recursive(vector<Cell>& maze, int numC, int start, int end, bool done);
-void BFS(vector<Cell>& maze, int numC, int start, int end);
-
 int main() {
+	TCHAR buffer[BUFSIZE];
+	DWORD dwRet = GetCurrentDirectory(BUFSIZE, buffer);
+	cout << "Current directory is: "<< buffer << endl;
 	ifstream In("maze.txt");//the file maze.txt in in same folder as the C++ source file.
 	int numR, numC;//number of rows, number of columns
 	In >> numR >> numC;
@@ -59,8 +68,6 @@ int main() {
 
 	if(numR<0 || numC<0) {
 		cout << "Input error: numR=" << numR << ", numC=" << numC << endl;
-		// std::filesystem::path cwd = std::filesystem::current_path();
-		// cout << "Current working directory is: " << cwd.string() << endl;
 		return -1;
 	}
 
@@ -80,35 +87,36 @@ int main() {
 	}
 	maze->begin = maze->mazemap.find(Start)->second;
 	maze->end = maze->mazemap.find(End)->second;
+
 	//Printing to file solution.txt, which will be in the same folder as maze.txt
 	ofstream Out("solution.txt");
 	DFS_iterative(mazeGiven, numC, Start, End);
 	DFS_recursive(mazeGiven, numC, Start, End, false);
 	BFS(mazeGiven, numC, Start, End);
 	MY_DFS_iterative(*maze, Out);
-	MY_DFS_recursive_wrap(*maze, Out);
-	MY_BFS(*maze, Out);
-	Out.close();
+	// MY_DFS_recursive_wrap(*maze, Out);
+	// MY_BFS(*maze, Out);
 	delete maze;
+	Out.close();
 	return 0;
 }
 
-void MY_DFS_iterative(Maze& maze, ostream& out){
+void MY_DFS_iterative(Maze& maze, ofstream& out){
 	stack<Cell*> S;
 	maze.reset();
 	Cell* robot = maze.begin;
 	maze.begin = maze.begin->from;
 	S.push(robot);
-	while(!S.empty() || !(maze.end->visited)) {
+	while(!S.empty() && !(maze.end->visited)) {
 		robot = S.top();
 		maze.path.push_back(robot);
 		S.pop();
 		if(!(robot->visited)){
 			robot->visited = true;
-			Cell* nb = robot->neighbor(maze);
-			while(!nb && !(nb->from)){ // connect unconnected node
-				nb->from = robot;
-				S.push(nb);
+			for(auto node : robot->neighbor(maze)){
+				if(node->from) continue;
+				node->from = robot;
+				S.push(node);
 			}
 		}
 	}
@@ -123,7 +131,7 @@ void DFS_iterative(vector<Cell>& maze, int numC, int start, int end){
 void DFS_recursive(vector<Cell>& maze, int numC, int start, int end, bool done){
 
 }
-void MY_DFS_recursive_wrap(Maze& maze, ostream& out){
+void MY_DFS_recursive_wrap(Maze& maze, ofstream& out){
 	maze.reset();
 	maze.begin->from = maze.begin;
 	out << "All Moves Using DFS_recursive:" << endl;
@@ -134,17 +142,17 @@ void MY_DFS_recursive_wrap(Maze& maze, ostream& out){
 void MY_DFS_recursive(Maze& maze, Cell* cell){
 	cell->visited = true;
 	maze.path.push_back(cell);
-	while(!(cell->neighbor(maze))){
-		cell->neighbor(maze)->from = cell;
-		cell = cell->neighbor(maze);
-		MY_DFS_recursive(maze, cell);
-	}
+	// while(!(cell->neighbor(maze))){
+	// 	cell->neighbor(maze)->from = cell;
+	// 	cell = cell->neighbor(maze);
+	// 	MY_DFS_recursive(maze, cell);
+	// }
 
 }
 void BFS(vector<Cell>& maze, int numC, int start, int end){
 
 }
-void MY_BFS(Maze& maze, ostream& out){
+void MY_BFS(Maze& maze, ofstream& out){
 	queue<Cell*> Q;
 	maze.reset();
 	Cell* robot = maze.begin;
@@ -156,11 +164,11 @@ void MY_BFS(Maze& maze, ostream& out){
 		Q.pop();
 		if(!(robot->visited)){
 			robot->visited = true;
-			Cell* nb = robot->neighbor(maze);
-			while(!nb && !(nb->from)){
-				nb->from = robot;
-				Q.push(nb);
-			}
+			// Cell* nb = robot->neighbor(maze);
+			// while(!nb && !(nb->from)){
+			// 	nb->from = robot;
+			// 	Q.push(nb);
+			// }
 		}
 	}
 	// cout = out;
@@ -177,9 +185,11 @@ Maze::~Maze(){
 	reset();
 	delete begin;
 	delete end;
-	for(auto pr : mazemap){
-		delete pr.second;
-		mazemap.erase(pr.first);
+	auto itr = mazemap.begin();
+	while(itr != mazemap.end()){
+		delete itr->second;
+		mazemap.erase(itr->first);
+		itr++;
 	}
 	mazemap.clear();
 }
@@ -187,7 +197,14 @@ void Maze::reset(){
 	path.clear();
 	solution.clear();
 }
-void Maze::print_path(ostream& out){
+bool Maze::boundary(const int id){
+	bool in;
+	if(id >= 0 && id < rows*cols)
+		in = true;
+	else in = false;
+	return in;
+}
+void Maze::print_path(ofstream& out){
 	auto itr = path.begin();
 	out << (*itr)->id;
 	itr++;
@@ -196,7 +213,7 @@ void Maze::print_path(ostream& out){
 		itr++;
 	}
 }
-void Maze::print_solution(ostream& out){
+void Maze::print_solution(ofstream& out){
 	Cell* robot;
 	if(!(end->visited))
 		out << "No solution." << endl;
@@ -211,6 +228,13 @@ void Maze::print_solution(ostream& out){
 		out << robot->id << endl;
 	}
 }
+Cell::Cell(){
+	// this = nullptr;
+	this->id = -1;
+	this->wall = -1;
+	this->from = nullptr;
+	this->visited = false;
+}
 Cell::Cell(const int id, const int wall){
 	this->id = id;
 	this->wall = wall;
@@ -221,26 +245,43 @@ Cell::Cell(const int id, const int wall){
 // Cells are in row-major order,
 // S-W-N-E = 2184, MSB-LSB 1101(2) = 7(10) 1 indicates wall, 0 for connection. 
 // S-W-N-E is the sequence to visit next neighbor as well
-Cell* Cell::neighbor(Maze& maze){ 
+vector<Cell*> Cell::neighbor(Maze& maze){ 
 	int markDec = wall;
 	string markBin = bitset<4>(markDec).to_string();
-	Cell* neighbor = nullptr;
-	for(int i=0; i<4; i++){
-		if(markBin[1] == '1'){ // S
-			neighbor = maze.mazemap.find(id+maze.cols)->second;
-		} else if(markBin[0] == '1'){ // W
-			neighbor = maze.mazemap.find(id-1)->second;
-		} else if(markBin[3] == '1'){ // N
-			neighbor = maze.mazemap.find(id-maze.cols)->second;
-		} else if(markBin[2] == '1'){ // E
-			neighbor = maze.mazemap.find(id+1)->second;
-		}
-		if(neighbor && !(neighbor->visited)) break;
-		else neighbor = nullptr;
+	vector<Cell*> neighbor;
+	Cell *S, *W, *N, *E;
+	S = W = N = E = nullptr;
+	if(maze.boundary(id+maze.cols))
+		S = maze.mazemap.find(id+maze.cols)->second;
+	if(maze.boundary(id-1))
+		W = maze.mazemap.find(id-1)->second;
+	if(maze.boundary(id-maze.cols))
+		N = maze.mazemap.find(id-maze.cols)->second;
+	if(maze.boundary(id+1))
+		E = maze.mazemap.find(id+1)->second;
+
+	if(markBin[1] == '0' && S){ // S
+		neighbor.push_back(S);
+	} if(markBin[0] == '0' && W){ // W
+		neighbor.push_back(W);
+	} if(markBin[3] == '0' && N){ // N
+		neighbor.push_back(N);
+	} if(markBin[2] == '0' && E){ // E
+		neighbor.push_back(E);
 	}
+	
+	for(auto node : {S, W, N, E}){
+		cout << node;
+	}
+	cout << endl;
 	return neighbor;
 }
-
+ostream& operator<<(ostream& str, const Cell* C){
+	if(!C) str << "nullptr; ";
+	else
+		str << "id="<< C->id << ", visited=" << C->visited << "; ";
+	return str;
+}
 /*
 the file maze.txt contains the following:
 
