51e7c0e571de71f69fd55f8a8f2e2640b57c8865
TigerBig <jizlin575@gmail.com>
2022-10-04 21:31:03 -0400

    update readme, include environment info

--- HW3/HW3.cpp
+++ HW3/HW3.cpp
@@ -16,27 +16,82 @@
 
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <list>
+#include <tuple>
+#include <stack>
+#include <functional>
+#include <climits>
+#include <type_traits>
+#include <windows.h>
+#include <tchar.h>
+
+#define BUFSIZE MAX_PATH
+
 using namespace std;
-
-
-void ShortestPath(vector<list<pair<int, int>>>& graph, vector<tuple<int, int, int>>& table, vector<tuple<int, int, int>> & heap, int source);//20 points
-void MST(vector<list<pair<int, int>>>& graph, vector<tuple<int, int, int>>& table, vector<tuple<int, int, int>> & heap, int source); //5 points
-
+void ShortestPath(vector<list<pair<int, int>>>& graph, vector<tuple<int, int, bool>>& table, vector<tuple<int, int>>& heap, int source);//20 points
+void MST(vector<list<pair<int, int>>>& graph, vector<tuple<int, int, bool>>& table, vector<tuple<int, int>>& heap, int source); //5 points
 
 int main() {
-  int source;
-  int cost; int from; int id; int hpindex;
+	TCHAR buffer[BUFSIZE];
+	DWORD dwRet = GetCurrentDirectory(BUFSIZE, buffer);
+	cout << endl << "Current directory is: "<< buffer << endl;
+
+	cout << endl << "C++ default standard in g++ compiler is: " << __cplusplus << endl;
+
+	ifstream In("graph.txt");
+	if(!In) cout << endl << "File is not existed.";
+	if(In.peek() == std::ifstream::traits_type::eof()) 
+		cout << endl << "Stream reaches eof, file is empty.";
+	// if(std::filesystem::is_empty("graph.txt")) cout << endl << "File is empty indicated by filesystem.";
+	// if(is_empty<ifstream>::In) cout << endl << "File is empty.";
+	// if(is_empty(In)) cout << endl << "File is empty indicated by filesystem.";
+	// if(is_empty<In>::value)
+	
+	// check input stream data
+	// char data;
+	// while(In >> data){
+	// 	cout << data;
+	// }
+  
+	int source;
+	int nodes, edges;
+	int src, dst, weight;
   vector<list<pair<int, int>>> graph;
   //your code to read in graph from "graph.txt"
 
-  vector<tuple<int, int, int>> table;//define your tuple
-  vector<tuple<int, int, int>> heap; //define your tuple
+  vector<tuple<int, int, bool>> table;//define your tuple
+  vector<tuple<int, int>> heap; //define your tuple
 
+  In >> source >> nodes >> edges;
+	In >> src;
+	for(int i=0; i<nodes; i++){
+		dst = weight = -1;
+		list<pair<int, int>> temp {};
+		// for(In >> src; src == i;){
+		// for(; src == i; In >> src){
+		while(src && src == i){
+			In >> dst >> weight;
+			temp.push_back(make_pair(dst, weight));
+			In >> src;
+		}
+		graph.push_back(temp);
+	}
+	In.close();
+	if(graph.size() == 0) cout << endl << "graph is empty" << endl;
 
-  
-
+	// print the graph
+	auto itr1 = graph.begin();
+	while(itr1 != graph.end()){ // list
+		auto itr2 = itr1->begin();
+		while(itr2 != itr1->end()){ // pair
+			cout << endl << source << " " << itr2->first << " " << itr2->second;
+			itr2++;
+		}
+		itr1++;
+		src++;
+	}
 
   //You might need to make some initialization setting for table and heap
 
@@ -46,12 +101,91 @@ int main() {
 
   MST(graph, table, heap, source);//HW2B
   
+	return 0;
 }
-void ShortestPath(vector<list<pair<int, int>>>& graph, vector<tuple<int, int, int>>& table, vector<tuple<int, int, int>> & heap, int source) {
+void ShortestPath(vector<list<pair<int, int>>>& graph, vector<tuple<int, int, bool>>& table, vector<tuple<int, int>>& heap, int source) {
+	cout << endl << "For the shortest paths using Dijkstra's algorithm:";
+	// initialization
+	int nodes;
+	nodes = graph.size();
+	for(int i=0; i<nodes; i++){
+		table.push_back(make_tuple(INT_MAX, -1, false));
+		// (cost, from, visited)
+	}
+	// if(source == NULL){
+	// 	cout << endl << "source node is not existed." << endl;
+	// 	return;
+	// } else 
+	if(source < 0){
+		cout << endl << "source node is a negtive value. source=" << source << endl;
+		return;
+	}
+	get<0>(table[source]) = 0;
+	get<1>(table[source]) = source;
+	get<2>(table[source]) = true;
+
+	// connecting
+	stack<int> S;
+	S.push(source);
+	while(!S.empty()){
+		int node = S.top();
+		S.pop();
+		if(get<2>(table[node])) continue; // node is visited
+		else get<2>(table[node]) = true;
+		
+		int neighborMin{-1};
+		auto itr = graph[node].begin();
+		while(itr != graph[node].end()){
+			// int neighbor = itr->first;
+			// int weight = itr->second;
+			// int costThis = get<0>(table[node]);
+			// int costNei = get<0>(table[neighbor]);
+			int neighbor = ref(itr->first);
+			if(get<2>(table[neighbor])) continue; // run only on unvisited neighbor
+			else S.push(neighbor);
+			int weight = ref(itr->second);
+			int costThis = ref(get<0>(table[node]));
+			int costNei = ref(get<0>(table[neighbor]));
+			if(costThis+weight < costNei){
+				costNei = costThis + weight;
+				get<1>(table[neighbor]) = node;
+				neighborMin = neighbor;
+			}
+		}
+		if(neighborMin == -1) continue;
+		else S.push(neighborMin);
+	}
+
+	// print the result from table
+	for(int i=0; i<nodes; i++){
+		cout << endl << "The cost from " << source << " to " << i << " is ";
+		if(get<1>(table[i]) == -1){
+			cout << "infinity and no paths exist!";
+		} else {
+			cout << get<0>(table[i]) << " and the path is   ";
+			int node{i};
+			cout << i;
+			while(node != source){
+				node = get<1>(table[node]);
+				cout << "<-" << node;
+			}
+		}
+
+	}
+
 
   //Your code  -- HW2A 20 pts
 }
-void MST(vector<list<pair<int, int>>>& graph, vector<tuple<int, int, int>>& table, vector<tuple<int, int, int>> & heap, int source) {
+void MST(vector<list<pair<int, int>>>& graph, vector<tuple<int, int, bool>>& table, vector<tuple<int, int>>& heap, int source) {
+	cout << endl << "For the minimum spanning tree using Prim's algorithm:";
+	if(!source){
+		cout << endl << "source node is not existed." << endl;
+		return;
+	} else if(source < 0){
+		cout << endl << "source node is a negtive value." << endl;
+		return;
+	}
+	cout << endl << "The selected edges are:";
 
   //Your code -- HW2B 5 pts
 }
--- README.md
+++ README.md
@@ -1,6 +1,50 @@
 # Algorithm-Practice
 Algorithm problem set and solutions written in C++
 
+## Environment
+### Windows
+Windows 11 Pro 21H2, OS build 22000.1042
+
+**Developed under Visual Studio 2022**
+<br>
+Access info from `Project Properties > Configuration Properties > C/C++ > General`
+  - Windows SDK Version, 10.0 (latest installed version)
+  - Platform Toolset, Visual Studio 2022 (v143)
+  - C++ Language Standard, ISO C++14 Standard
+  - C Language Standard, Legacy MSVC
+
+**Developed under Visual Studio Code**
+```
+> msys\usr\bin\g++.exe -fdiagnostics-color=always -g SOURCE -o OBJECT
+> g++ --version
+g++ (GCC) 11.2.0
+Copyright (C) 2021 Free Software Foundation, Inc.
+> gcc --version
+gcc (GCC) 11.2.0
+Copyright (C) 2021 Free Software Foundation, Inc.
+> g++ -x c++ -E -dM /dev/null | grep -F __cplusplus
+#define __cplusplus 201703L
+```
+
+### MacOS
+Monterey 12.6 
+
+**Developed under Visual Studio Code**
+```
+> /usr/bin/g++ -fdiagnostics-color=always -g SOURCE -o OBJECT -std=c++17
+> g++ --version
+> gcc --version
+Apple clang version 14.0.0 (clang-1400.0.29.102)
+Target: arm64-apple-darwin21.6.0
+Thread model: posix
+InstallerDir: /Library/Developer/CommandLineTools/usr/bin
+> g++ -x c++ -E -dM /dev/null | grep -F __cplusplus
+#define __cplusplus 199711L
+```
+## Note
+- `<windows.h> , <tchar.h>` are included to find out the current working directory in Windows Platform
+- `error 'iostream' file not found.` Configuration file is not correctly set up in local machine.
+- `ssh: connect to host github.com port 22: Operation timed out.` Local network blocked the request from ssh connection
 ## Acknoledge
 - Classnote is provided by professor Roger Chen who is the instructor of course CSE674\
 https://ecs.syracuse.edu/faculty-staff/c-y-roger-chen
